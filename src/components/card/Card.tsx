706bf4eb86903f10433d284d24572c496812a65d
MAXIMUS\shawn <shawnbahramy@gmail.com>
2022-04-02 16:18:35 -0400

    styling

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -5,19 +5,8 @@ import './card.css';
 
 export const Card = (props: {card: ICard}) => {
 
-
-    const getCustomStyles = () => {
-        const styles: string[] = [];
-        if (props.card.isPlayerCard)
-            styles.push('player-ownership')
-        if (props.card.isSelected)
-            styles.push('selected');
-
-        return styles.join(' ');
-    }
-
     return (
-        <div className={`card ` + getCustomStyles()}>
+        <div className='card'>
             <div className='card-header'>
                 <CardRanks ranks={props.card.ranks} />
             </div>
